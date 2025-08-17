b2489fe24ec27df4870d6652035c7d7d54a508df
46768 <46768@bodin.ac.th>
2025-08-17 20:08:49 +0700

    feat: InputField ui input component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import InputField from "@/components/ui/input/InputField";
+
+export default function Test() {
+	return (
+		<div>
+			<main>
+				<div>
+					<InputField
+						onClick={()=>{}}
+						legend="Test"
+						buttonText="OK"
+					/>
+				</div>
+			</main>
+		</div>
+	);
+}
--- src/components/ui/input/InputField.tsx
+++ src/components/ui/input/InputField.tsx
@@ -1,1 +1,25 @@
-export default function InputField() {}
+import { MouseEventHandler } from "react";
+
+export default function InputField({
+	onClick,
+
+	legend,
+	buttonText,
+	placeholder = "placeholder",
+}: Readonly<{
+	onClick: MouseEventHandler<HTMLButtonElement>,
+
+	legend: string,
+	buttonText: string,
+	placeholder?: string,
+}>) {
+	return (
+		<fieldset className="fieldset bg-base-200 border border-base-300 rounded-box p-4 w-xs">
+			<legend className="fieldset-legend">{legend}</legend>
+			<div className="join">
+				<input className="input join-item" placeholder={placeholder}/>
+				<button className="btn join-item" onClick={onClick}>{buttonText}</button>
+			</div>
+		</fieldset>
+	);
+}
