abf8520f25eec8de6437d362be823863687b3911
46768 <46768@bodin.ac.th>
2025-08-17 20:33:20 +0700

    feat: add navigation ui components

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import InputField from "@/components/ui/input/InputField";
+import NavBar from "@/components/ui/nav/NavBar";
+import NavLink from "@/components/ui/nav/NavLink";
 
 export default function Test() {
 	return (
 		<div>
 			<main>
-				<div>
+				<div className="flex flex-col gap-4">
 					<InputField
 						onClick={()=>{}}
 						legend="Test"
 						buttonText="OK"
 					/>
+
+					<NavLink href="/test">Test</NavLink>
+					<NavLink href="/test/non">Test Non</NavLink>
+
+					<NavBar/>
 				</div>
 			</main>
 		</div>
--- src/components/ui/nav/NavLink.tsx
+++ src/components/ui/nav/NavLink.tsx
@@ -1,1 +1,16 @@
-export default function NavLink() {}
+import Link from "next/link";
+
+export default function NavLink({
+	href,
+	children,
+}: Readonly<{
+	href: string,
+
+	children: React.ReactNode,
+}>) {
+	return (
+		<Link className="btn w-max bg-base-300" href={href}>
+			{children}
+		</Link>
+	);
+}
